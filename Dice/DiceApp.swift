98c2f07034aa5e574e70af68a60f32ca4319f627
kento.yamazaki <kento.yamazaki@kentoyamazakideMacBook-Air.local>
2024-03-14 09:14:23 +0900

    App: Initial commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by kento.yamazaki on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
---
+++ Packages/DiceContent/Sources/DiceContent/DiceContent.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+/// Bundle for the DiceContent project
+public let diceContentBundle = Bundle.module
