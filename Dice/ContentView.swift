3261db76bdbe35129d8759713b98d8e0778e8f82
kento.yamazaki <other_usage@icloud.com>
2024-03-25 01:47:38 +0900

    Feat: Add an immersive space opner

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -5,19 +5,56 @@
 //  Created by kento.yamazaki on 2024/03/09.
 //
 
-import SwiftUI
-import RealityKit
+import ARKit
+import Combine
 import DiceContent
+import RealityKit
+import simd
+import SwiftUI
 
 struct ContentView: View {
 
+    let session = ARKitSession()
+    let worldInfoProvider = WorldTrackingProvider()
+    @State var scene = Entity()
     @State var enlarge = false
+    @State var sceneUpdateSubscription : Cancellable? = nil
 
     var body: some View {
+        
+        
+        
         VStack {
-            RealityView { content in
+            RealityView {  content in
+                
+                try? await session.run([worldInfoProvider])
+                
+                // Called per every frame
+                sceneUpdateSubscription = content.subscribe(to: SceneEvents.Update.self) { event in
+                    guard let pose = worldInfoProvider.queryDeviceAnchor(atTimestamp: CACurrentMediaTime()) else { return }
+                    let dt = event.deltaTime
+                    let toDeviceTransform = pose.originFromAnchorTransform
+                    print("dt: \(dt), toDeviceTransform: \(toDeviceTransform)")
+//                    let devicePosition = toDeviceTransform.translation
+//                    deviceRotation = toDeviceTransform.upper3x3
+//                    scene.position = devicePosition
+                    scene.transform = .init(
+                        matrix: 
+                                .init(toDeviceTransform.columns.0,
+                                      toDeviceTransform.columns.1,
+                                      toDeviceTransform.columns.2,
+                                      .init(
+                                        x: toDeviceTransform.columns.3.x, 
+                                        y: toDeviceTransform.columns.3.y,
+                                        z: toDeviceTransform.columns.3.z - 1.0, w: toDeviceTransform.columns.3.w)
+                                     )
+                    )
+                    
+                } as? any Cancellable
+                
                 // Add the initial RealityKit content
                 if let scene = try? await Entity(named: "Scene", in: diceContentBundle) {
+                    self.scene = scene
                     content.add(scene)
                 }
             } update: { content in
