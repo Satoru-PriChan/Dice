bd535ae86656823d1242eb83db62b4bdd0d35dba
kento.yamazaki <other_usage@icloud.com>
2024-06-25 14:44:24 +0900

    Chore: Add Test

---
+++ DiceTests/DiceTest.swift
@@ -0,0 +1,17 @@
+//
+//  DiceTest.swift
+//  DiceTests
+//
+//  Created by kento.yamazaki on 2024/06/23.
+//
+
+import Testing
+
+struct DiceTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        
+    }
+
+}
