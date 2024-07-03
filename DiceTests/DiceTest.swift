96fbf1fabc8110c685bc1f85ff51545df9e92a66
kento.yamazaki <other_usage@icloud.com>
2024-07-03 17:04:18 +0900

    Test: Add test for random spin

--- DiceTests/DiceTest.swift
+++ DiceTests/DiceTest.swift
@@ -2,16 +2,40 @@
 //  DiceTest.swift
 //  DiceTests
 //
-//  Created by kento.yamazaki on 2024/06/23.
+//  Created by kento.yamazaki on 2024/07/03.
 //
 
 import Testing
+@testable import Dice
 
-struct DiceTest {
+@Suite("Tests of ContentViewModel")
+actor DiceContentViewModelTestActor {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        
+    @Test
+    func randomeSpin() async throws {
+        let viewModel = await ContentViewModel()
+        let model = await viewModel.model
+        // Expect the initial value is five
+        #expect(model.spin == .five)
+        var result: [ContentDiceSpinStrategy] = []
+        // Test ratio of each side of dice after 1000 random spins
+        for _ in 0...999 {
+            await viewModel.onTapDiceEntity()
+            let model = await viewModel.model
+            result.append(model.spin)
+        }
+        for target in ContentDiceSpinStrategy.allCases {
+            #expect(
+                result.count(where: { spin in
+                    if case target = spin {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+                 >= 10
+            )
+        }
     }
 
 }
